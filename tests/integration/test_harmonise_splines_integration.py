9c55032a5e32f67db837fd3d99a6f38c47d60eeb
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-03-20 07:39:47 +0100

    Add failing tests of spline harmonisation

--- tests/integration/test_harmonise_splines_integration.py
+++ tests/integration/test_harmonise_splines_integration.py
@@ -17,7 +17,6 @@ In this module, we need to test a few things:
 import numpy as np
 import pytest
 
-from gradient_aware_harmonisation.spline import SplineScipy
 from gradient_aware_harmonisation.utils import Timeseries, harmonise_splines
 
 scipy = pytest.importorskip("scipy")
@@ -74,21 +73,27 @@ def check_continuity(  # noqa: PLR0913
 @pytest.mark.parametrize("convergence_time", (None, 3.0))
 def test_target_and_harmonisee_equal(convergence_time, harmonisation_time):
     time_axis = np.array([0.0, 1.0, 2.0, 3.0])
-    target = Timeseries(time_axis=time_axis, values=time_axis**2).to_spline()
+    target_ts = Timeseries(time_axis=time_axis, values=time_axis**2)
+    target = target_ts.to_spline()
 
-    harmon_spline = harmonise_splines(
+    harmonised_spline = harmonise_splines(
         target=target,
         harmonisee=target,
         harmonisation_time=harmonisation_time,
         convergence_time=convergence_time,
     )
 
+    if convergence_time is None:
+        convergence_time_exp = target_ts.time_axis.max()
+    else:
+        convergence_time_exp = convergence_time
+
     check_continuity(
-        harmonised=harmon_spline,
+        harmonised=harmonised_spline,
         target=target,
         harmonisation_time=harmonisation_time,
         convergence_spline=target,
-        convergence_time=convergence_time,
+        convergence_time=convergence_time_exp,
     )
 
 
@@ -96,40 +101,34 @@ def test_target_and_harmonisee_equal(convergence_time, harmonisation_time):
 @pytest.mark.parametrize("convergence_time", (None, 3.0))
 def test_target_and_harmonisee_differ(convergence_time, harmonisation_time):
     time_axis_target = np.array([0.0, 1.0, 2.0, 3.0])
-    timeseries_target = Timeseries(
+    target = Timeseries(
         time_axis=time_axis_target, values=time_axis_target ** (1 / 2)
-    )
+    ).to_spline()
 
     time_axis_harmonisee = np.array([3.0, 4.0, 5.0, 6.0])
-    timeseries_harmonisee = Timeseries(
+    harmonisee_ts = Timeseries(
         time_axis=time_axis_harmonisee, values=-1.3 * np.sin(time_axis_harmonisee) + 8
     )
+    harmonisee = harmonisee_ts.to_spline()
 
-    splines = dict(
-        target=SplineScipy(
-            scipy.interpolate.make_interp_spline(
-                timeseries_target.time_axis, timeseries_target.values
-            )
-        ),
-        harmonisee=SplineScipy(
-            scipy.interpolate.make_interp_spline(
-                timeseries_harmonisee.time_axis, timeseries_harmonisee.values
-            )
-        ),
-    )
-
-    harmon_spline = harmonise_splines(
-        target=splines["target"],
-        harmonisee=splines["harmonisee"],
+    harmonised_spline = harmonise_splines(
+        target=target,
+        harmonisee=harmonisee,
         harmonisation_time=harmonisation_time,
         convergence_time=convergence_time,
     )
 
+    if convergence_time is None:
+        convergence_time_exp = harmonisee_ts.time_axis.max()
+    else:
+        convergence_time_exp = convergence_time
+
     check_continuity(
-        test_criterion=test_criterion,
+        harmonised=harmonised_spline,
+        target=target,
         harmonisation_time=harmonisation_time,
-        harmonised=harmon_spline,
-        target=splines["target"],
+        convergence_spline=harmonisee,
+        convergence_time=convergence_time_exp,
     )
 
 
@@ -139,41 +138,35 @@ def test_more_realistic(convergence_time, harmonisation_time):
     """
     Both testing more realistic data but also a time axis that has integer values
     """
-    timeseries_target = Timeseries(
+    target = Timeseries(
         time_axis=np.array([2000, 2001, 2002, 2003]),
         values=np.array([371.77, 373.72, 376.33, 378.43]),
-    )
+    ).to_spline()
 
-    timeseries_harmonisee = Timeseries(
+    harmonisee_ts = Timeseries(
         time_axis=np.array([2003, 2004, 2005, 2006]),
         values=np.array([376.28, 378.83, 381.20, 382.55]),
     )
+    harmonisee = harmonisee_ts.to_spline()
 
-    splines = dict(
-        target=SplineScipy(
-            scipy.interpolate.make_interp_spline(
-                timeseries_target.time_axis, timeseries_target.values
-            )
-        ),
-        harmonisee=SplineScipy(
-            scipy.interpolate.make_interp_spline(
-                timeseries_harmonisee.time_axis, timeseries_harmonisee.values
-            )
-        ),
-    )
-
-    harmon_spline = harmonise_splines(
-        target=splines["target"],
-        harmonisee=splines["harmonisee"],
+    harmonised_spline = harmonise_splines(
+        target=target,
+        harmonisee=harmonisee,
         harmonisation_time=harmonisation_time,
         convergence_time=convergence_time,
     )
 
+    if convergence_time is None:
+        convergence_time_exp = harmonisee_ts.time_axis.max()
+    else:
+        convergence_time_exp = convergence_time
+
     check_continuity(
-        test_criterion=test_criterion,
+        harmonised=harmonised_spline,
+        target=target,
         harmonisation_time=harmonisation_time,
-        harmonised=harmon_spline,
-        target=splines["target"],
+        convergence_spline=target,
+        convergence_time=convergence_time_exp,
     )
 
 
