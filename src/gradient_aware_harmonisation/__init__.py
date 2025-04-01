d87527940d55727fe021ae4beb5893e857bc3350
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-04-01 13:36:34 +0200

    reduce line length in documentation

--- src/gradient_aware_harmonisation/__init__.py
+++ src/gradient_aware_harmonisation/__init__.py
@@ -60,7 +60,8 @@ def harmonise(  # noqa: PLR0913
         at the last time point in harmonisee_timeseries.
 
     get_harmonised_spline
-        Function used to get the harmonised spline from a gradient-preserving spline and the timeseries to converge to
+        Function used to get the harmonised spline from a gradient-
+        preserving spline and the timeseries to converge to
 
     Returns
     -------
