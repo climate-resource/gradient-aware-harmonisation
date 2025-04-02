68de35b3b67ad10b695beb672939b17ddeb41e76
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-04-02 14:58:01 +0200

    add assert statement

--- tests/integration/test_convergence_integration.py
+++ tests/integration/test_convergence_integration.py
@@ -118,7 +118,8 @@ def test_cosine_decay_spline_derivative():
     np.testing.assert_equal(spline(6.0), cos_deriv(6.0, 2.0, 10.0))
     np.testing.assert_equal(spline(3.0), cos_deriv(3.0, 2.0, 10.0))
     # ensure correct computation of derivative itself
-    scipy.integrate.quad(spline, 2.0, 10.0)
+    integral, _ = scipy.integrate.quad(spline, 2.0, 10.0)
+    np.testing.assert_allclose(integral, -1.0)
 
     # Array input
     np.testing.assert_equal(
@@ -155,7 +156,8 @@ def test_cosine_decay_spline_derivative_apply_to_convergence():
     np.testing.assert_equal(spline(6.0), -cos_deriv(6.0, 2.0, 10.0))
     np.testing.assert_equal(spline(3.0), -cos_deriv(3.0, 2.0, 10.0))
     # ensure correct computation of derivative itself
-    scipy.integrate.quad(spline, 2.0, 10.0)
+    integral, _ = scipy.integrate.quad(spline, 2.0, 10.0)
+    np.testing.assert_allclose(integral, -1.0)
 
     # Array input
     np.testing.assert_equal(
