f3c84b8f1a9bd866c4d48dce780fc868c9c9ca7d
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-04-24 07:29:26 +0200

    add documentation to cubic spline functions

--- src/gradient_aware_harmonisation/add_cubic.py
+++ src/gradient_aware_harmonisation/add_cubic.py
@@ -14,6 +14,28 @@ from gradient_aware_harmonisation.typing import NP_ARRAY_OF_FLOAT_OR_INT
 def taylor_shift(
     coeffs: NP_ARRAY_OF_FLOAT_OR_INT, shift: float
 ) -> NP_ARRAY_OF_FLOAT_OR_INT:
+    """
+    Compute Taylor shift according to Shaw and Traub (1974)
+
+    Parameters
+    ----------
+    coeffs
+        polynomial coefficients
+
+    shift
+        Taylor shift
+
+    Returns
+    -------
+    :
+        new polynomial coefficients by Taylor shift
+
+    References
+    ----------
+    Shaw, M., & Traub, J. F. (1974). On the number of multiplications
+    for the evaluation of a polynomial and some of its derivatives.
+    Journal of the ACM, 21(1), 161-167.
+    """
     if shift == 0.0:
         return coeffs
 
@@ -49,6 +71,70 @@ def harmonise_splines_add_cubic(
     harmonisation_time: float | int,
     convergence_time: float | int,
 ) -> Spline:
+    """
+    Generate cubic spline
+
+    The cubic spline interpolates between harmonised
+    spline and harmonisee
+
+    Parameters
+    ----------
+    diverge_from
+        Spline whose value and first derivative at the
+        harmonization time match the target spline values.
+
+    harmonisee
+        Spline that the cubic spline should converge to
+        and match at and after the convergence time.
+
+    harmonisation_time
+        Time point at which cubic spline should be
+        matched to the target.
+
+    convergence_time
+        Time point at which the cubic spline should
+        converge to harmonisee.
+
+    Returns
+    -------
+    :
+        cubic spline
+
+    Examples
+    --------
+    >>> from gradient_aware_harmonisation.timeseries import Timeseries
+    >>> from gradient_aware_harmonisation.add_cubic import harmonise_splines_add_cubic
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> import matplotlib.pyplot as plt
+
+    >>> harmonisation_time = 3.2
+    >>> convergence_time = 3.8
+    >>> time = np.arange(3.0, 4.1, step=0.1)
+
+    >>> harmonised = Timeseries(time_axis=time, values=-0.5 * time + 1)
+    >>> harmonisee = Timeseries(time_axis=time, values=0.2 * time**3 - 5)
+    >>> cubic_spline = harmonise_splines_add_cubic(
+    ...    diverge_from=harmonised.to_spline(),
+    ...    harmonisee=harmonisee.to_spline(),
+    ...    harmonisation_time=harmonisation_time,
+    ...    convergence_time=convergence_time
+    ...)
+
+    >>> _, ax = plt.subplots(figsize=(6, 3))
+    >>> for y, name in zip(
+    ...     [harmonised.values, harmonisee.values, cubic_spline(time)],
+    ...     ["diverge_from", "harmonisee", "cubic_spline"],
+    ... ):
+    ...     ax.plot(time, y, label=name)
+    >>> ax.axvline(harmonisation_time, color="black", linestyle="--")
+    >>> ax.axvline(convergence_time, color="black", linestyle="--")
+    >>> ax.legend()
+    >>> plt.show()
+
+    ![](../../../assets/images/cubic_spline.png)
+
+    """
     try:
         import scipy.interpolate
     except ImportError as exc:
